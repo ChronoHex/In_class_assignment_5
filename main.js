dc80566b27a4e9b4441c2e7f507cbd9d4411aa5c
Eugene Rodriguez <er11@usf.edu>
2025-03-27 14:52:12 -0400

    in class assignment

---
+++ main.js
@@ -0,0 +1,20 @@
+import { getPosts } from "./api";
+
+function displayPosts() {
+    const container = document.getElementById('postContainer');
+
+    getPosts()
+        .then(posts => {
+            posts.slice(0, 5).forEach(post => {
+                const div = document.createElement('div');
+                div.innerHTML = `<h3>${post.name}</h3><p>${post.email}</p>`;
+                container.appendChild(div);
+            });
+        })
+        .catch(error => {
+        container.innerHTML = `<p style="color: red;">Failed to load posts: ${error.message}</p>`;
+        })
+        
+}
+
+displayPosts();
